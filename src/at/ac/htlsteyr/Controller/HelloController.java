47c91e5a7e53bb11ff7655e2d99cc9d3b895a5d1
Christian42 <cchimani@htl-steyr.ac.at>
2022-02-11 08:51:26 +0100

    a

--- src/at/ac/htlsteyr/Controller/HelloController.java
+++ src/at/ac/htlsteyr/Controller/HelloController.java
@@ -1,11 +1,19 @@
 package at.ac.htlsteyr.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
     public GridPane test;
+    public Button werfen1;
+    public Button werfen2;
+    public Button werfen3;
+    public Button werfen4;
+    public Button werfen5;
+    public Button werfen6;
+    public Button werfen7;
 
     @FXML
     protected void onHelloButtonClick() {
