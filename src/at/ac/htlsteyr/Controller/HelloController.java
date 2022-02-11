286d569ed985696a86dbc68f5049e58e0951d586
nmekina <95867724+nmekina@users.noreply.github.com>
2022-02-11 09:15:47 +0100

    Console auf neuesten Stand gebracht

--- src/at/ac/htlsteyr/Controller/HelloController.java
+++ src/at/ac/htlsteyr/Controller/HelloController.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 
 public class HelloController {
-    public GridPane test;
     public Button werfen1;
     public Button werfen2;
     public Button werfen3;
@@ -15,9 +14,48 @@ public class HelloController {
     public Button werfen5;
     public Button werfen6;
     public Button werfen7;
+    public Circle k01;
+    public Circle k02;
+    public Circle k03;
+    public Circle k04;
+    public Circle k05;
+    public Circle k06;
+    public Circle k11;
+    public Circle k12;
+    public Circle k13;
+    public Circle k14;
+    public Circle k15;
+    public Circle k16;
+    public Circle k21;
+    public Circle k22;
+    public Circle k23;
+    public Circle k24;
+    public Circle k25;
+    public Circle k26;
+    public Circle k31;
+    public Circle k32;
+    public Circle k33;
+    public Circle k34;
+    public Circle k35;
+    public Circle k36;
+    public Circle k41;
+    public Circle k42;
+    public Circle k43;
+    public Circle k44;
+    public Circle k45;
+    public Circle k46;
+    public Circle k51;
+    public Circle k52;
+    public Circle k53;
+    public Circle k54;
+    public Circle k55;
+    public Circle k56;
+    public Circle k61;
     public Circle k62;
     public Circle k63;
     public Circle k64;
+    public Circle k65;
+    public Circle k66;
 
     @FXML
     protected void onHelloButtonClick() {
