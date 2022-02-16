64ef13763da945c032d67c6e160a9cf938cb6909
Christian42 <cchimani@htl-steyr.ac.at>
2022-02-16 14:55:19 +0100

    a

--- src/at/ac/htlsteyr/HelloApplicationGUI.java
+++ src/at/ac/htlsteyr/HelloApplicationGUI.java
@@ -30,25 +30,25 @@ HelloApplicationGUI extends Application {
         stage.setScene(scene);
         stage.show();
 
-        
+
         Spieler spieler1 = new Spieler();
         Spieler spieler2 = new Spieler();
         Spiel s = new Spiel();
         FeldView fw = new FeldViewConsole();
 
-        /*
+
         System.out.println("Name: Spieler1: ");
         Scanner scannerspieler1 = new Scanner(System.in);
         spieler1.setNickname(scannerspieler1.next());
-        */
+
 
 
         do {
-            /*
+
             System.out.println("Name: Spieler2: ");
             Scanner scannerspieler2 = new Scanner(System.in);
             spieler2.setNickname(scannerspieler2.next());
-             */
+
         } while (spieler1.getNickname().equals(spieler2.getNickname()));
 
 
