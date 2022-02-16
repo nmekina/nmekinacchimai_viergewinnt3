10fcfe58fba133e004ed0a48e00dad4c905c7c22
nmekina <95867724+nmekina@users.noreply.github.com>
2022-02-16 15:15:37 +0100

    Hello Controller

--- src/at/ac/htlsteyr/Controller/HelloController.java
+++ src/at/ac/htlsteyr/Controller/HelloController.java
@@ -1,5 +1,10 @@
 package at.ac.htlsteyr.Controller;
 
+import at.ac.htlsteyr.Model.Feld;
+import at.ac.htlsteyr.Model.Spiel;
+import at.ac.htlsteyr.Model.Spieler;
+import at.ac.htlsteyr.View.FeldView;
+import at.ac.htlsteyr.View.FeldViewGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +14,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+
+import java.util.Scanner;
 
 public class HelloController {
     public Button werfen1;
@@ -91,7 +98,127 @@ public class HelloController {
     public void buttonclick7(ActionEvent actionEvent) {
     }
 
-    public void confirm(ActionEvent actionEvent) {
+    public void confirm() {
+        Spieler spieler1 = new Spieler();
+        Spieler spieler2 = new Spieler();
+        Spiel s = new Spiel();
+        FeldView fw = new FeldViewGUI();
+
+        spieler1.setNickname("Nico");
+
+        do {
+
+            spieler2.setNickname("Chri");
+
+        } while (spieler1.getNickname().equals(spieler2.getNickname()));
+
+
+        s.zufallsgenerator();
+
+        spieler1.setSpielstein('x');
+        spieler2.setSpielstein('o');
+
+        /*
+        if (Spiel.spieler1 == true) {
+            System.out.println(spieler1.getNickname() + " beginnt: Spielstein(x/o) wählen: ");
+            Scanner scannerspielstein = new Scanner(System.in);
+
+
+            if (scannerspielstein.next().charAt(0) == 'x') {
+                spieler1.setSpielstein('x');
+                spieler2.setSpielstein('o');
+            } else {
+                spieler1.setSpielstein('o');
+                spieler2.setSpielstein('x');
+            }
+
+        } else if (Spiel.spieler2 == true) {
+
+            System.out.println(spieler2.getNickname() + " beginnt: Spielstein(x/o) wählen: ");
+            Scanner scannerspielstein = new Scanner(System.in);
+
+
+            if (scannerspielstein.next().charAt(0) == 'x') {
+                spieler2.setSpielstein('x');
+                spieler1.setSpielstein('o');
+            } else {
+                spieler2.setSpielstein('o');
+                spieler1.setSpielstein('x');
+            }
+
+        }
+        */
+
+
+
+/*
+        while (s.checkwin() == false) {
+            fw.display(spieler1, spieler2);
+            spalteneingabe(spieler1, spieler2);
+            s.werfen();
+            s.spielertausch();
+        }
+
+ */
+
+
+
+        if (Spiel.spieler1) {
+
+        } else {
+            //System.out.println(spieler2.getNickname() + " hat gewonnen!");
+        }
+
+        /*
+        System.out.println("Wollen Sie erneut spielen? (Ja(j), Nein(nicht j)");
+        Scanner scannerneustart = new Scanner(System.in);
+        if (scannerneustart.next().charAt(0) == 'j') {
+            Feld.spalten = 0;
+            Feld.spielfeld = new int[6][7];
+            Spiel.füllungspalten = new int [7];
+            launch();
+        }
+         */
+    }
+
+    public void spalteneingabe(Spieler spieler1, Spieler spieler2) {
+        Feld.spalten = 0;
+        String EingabeA;
+
+        if (Spiel.spieler1 == true) {
+
+            System.out.println(spieler1.getNickname() + " bitte Spaltennummer angeben: ");
+            Scanner sc = new Scanner(System.in);
+            EingabeA = sc.next();
+        } else {
+            System.out.println(spieler2.getNickname() + " bitte Spaltennummer angeben: ");
+            Scanner sc = new Scanner(System.in);
+            EingabeA = sc.next();
+        }
+
+        if (EingabeA.charAt(0) == 'R') {
+            Feld.spalten = 0;
+            Feld.spielfeld = new int[6][7];
+            Spiel.füllungspalten = new int [7];
+            confirm();
+        }
+
+        if (!(EingabeA.charAt(0) > '0' && EingabeA.charAt(0) < '8')) {
+            System.out.println("Achtung! Zahlen eingeben");
+            spalteneingabe(spieler1, spieler2);
+        } else {
+            Feld.spalten = Integer.parseInt(EingabeA);
+
+            if (Feld.spalten < 1 || Feld.spalten > 7) {
+                System.out.println("Achtung! Die eingegebene Spaltennummer muss zwischen 1 und 7 liegen!");
+                spalteneingabe(spieler1, spieler2);
+            } else if (Spiel.füllungspalten[Feld.spalten - 1] > 5) {
+                System.out.println("Spalte voll!!!!");
+                spalteneingabe(spieler1, spieler2);
+            } else {
+                Spiel.füllungspalten[Feld.spalten - 1]++;
+            }
+        }
     }
 
     public void blau(ActionEvent actionEvent) {
@@ -101,5 +228,8 @@ public class HelloController {
     }
 
     public void ueberschrift(MouseEvent mouseEvent) {
+    }
+
+    public void nameauswahl(ActionEvent actionEvent) {
     }
 }
